03efffe2b605fb14d258896a5d605b8c19fe4b78
Per Fuchs <per@shiftleft.io>
2018-06-21 15:17:25 +0200

    Redirects output to file

--- chandyMisra/runChandyMisra.sh
+++ chandyMisra/runChandyMisra.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 CLASSPATH=lib/cell1D.jar \
 prun -np $1 \
-/home/pfs250/chandyMisra/runForSingleNode.sh $2 $(($1 * $2))
+/home/pfs250/chandyMisra/runForSingleNode.sh $2 $(($1 * $2)) > out.log
