7ed69e19c31f16656ddda0b0be34fa38727e9dcb
Per Fuchs <per@shiftleft.io>
2018-07-23 11:50:13 +0200

    Adds automatic zipping

--- ibisImplementation/runChandyMisra.sh
+++ ibisImplementation/runChandyMisra.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+rm -f "/var/scratch/pfs250/safraExperiment/$4.zip"
 CLASSPATH=lib/cell1D.jar \
 prun -np $1 $7 \
 /home/pfs250/ibisImplemenation/runForSingleNode.sh $2 $(($1 * $2)) $3 "/var/scratch/pfs250/safraExperiment/$4" $5 $6 > out.log
+zip -r "/var/scratch/pfs250/safraExperiment/$4.zip" "/var/scratch/pfs250/safraExperiment/$4"
