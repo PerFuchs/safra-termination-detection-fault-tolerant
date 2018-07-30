85fc73ea3cf04913858ec49e423afca446484c1e
Per Fuchs <per@shiftleft.io>
2018-07-30 13:08:26 +0200

    Writes date and exact command run into out log

--- ibisImplementation/runChandyMisra.sh
+++ ibisImplementation/runChandyMisra.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 rm -f "/var/scratch/pfs250/safraExperiment/$4.zip"
+date > out.log
+echo $0 $@ >> out.log
 CLASSPATH=lib/cell1D.jar \
 prun -np $1 $8 \
-/home/pfs250/ibisImplemenation/runForSingleNode.sh $2 $(($1 * $2)) $3 "/var/scratch/pfs250/safraExperiment/$4" $5 $6 $7 > out.log
+/home/pfs250/ibisImplemenation/runForSingleNode.sh $2 $(($1 * $2)) $3 "/var/scratch/pfs250/safraExperiment/$4" $5 $6 $7 >> out.log
 zip -r "/var/scratch/pfs250/safraExperiment/$4.zip" "/var/scratch/pfs250/safraExperiment/$4"
