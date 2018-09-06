0354ff4e86c2bf1fb68375370d03075e9b1c45b0
Per Fuchs <per@shiftleft.io>
2018-09-06 13:22:25 +0200

    Afek-Kuten-Yung implementation

--- ibisImplementation/src/ibis/ipl/apps/safraExperiment/afekKuttenYung/AfekKuttenYungData.java
+++ ibisImplementation/src/ibis/ipl/apps/safraExperiment/afekKuttenYung/AfekKuttenYungData.java
@@ -3,9 +3,21 @@ package ibis.ipl.apps.safraExperiment.afekKuttenYung;
 import ibis.ipl.apps.safraExperiment.communication.Message;
 
 public class AfekKuttenYungData {
+  public static final int EMPTY_DIRECTION = -1;
+  public static final int ASK = 1;
+  public static final int GRANT = 2;
+
+  public static final int EMPTY_NODE = -1;
+  public static final int EMPTY_PARENT = EMPTY_NODE;
+
   public int parent;
-  public int distance;
   public int root;
+  public int distance;
+
+  public int req;
+  public int from;
+  public int to;
+  public int direction;
 
   public static AfekKuttenYungData getRandomData() {
     return null;
--- ibisImplementation/src/ibis/ipl/apps/safraExperiment/afekKuttenYung/AfekKuttenYungRunningState.java
+++ ibisImplementation/src/ibis/ipl/apps/safraExperiment/afekKuttenYung/AfekKuttenYungRunningState.java
@@ -2,6 +2,7 @@ package ibis.ipl.apps.safraExperiment.afekKuttenYung;
 
 import ibis.ipl.apps.safraExperiment.awebruchSyncronizer.AlphaSynchronizer;
 import ibis.ipl.apps.safraExperiment.awebruchSyncronizer.AwebruchClient;
+import ibis.ipl.apps.safraExperiment.chandyMisra.DistanceMessage;
 import ibis.ipl.apps.safraExperiment.communication.CommunicationLayer;
 import ibis.ipl.apps.safraExperiment.communication.Message;
 import ibis.ipl.apps.safraExperiment.experiment.Event;
@@ -11,8 +12,7 @@ import ibis.ipl.apps.safraExperiment.safra.api.TerminationDetectedTooEarly;
 import ibis.ipl.apps.safraExperiment.utils.OurTimer;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class AfekKuttenYungRunningState extends AfekKuttenYungState implements Runnable, AwebruchClient {
@@ -30,8 +30,10 @@ public class AfekKuttenYungRunningState extends AfekKuttenYungState implements R
   private boolean active;
   private boolean terminated;
   private Thread loopThread;
+  private int me;
 
   AfekKuttenYungRunningState(CommunicationLayer communicationLayer, Safra safra, AfekKuttenYungStateMachine afekKuttenYungMachine) {
+    me = communicationLayer.getID();
     synchronizer = new AlphaSynchronizer(communicationLayer, this);
 
     this.communicationLayer = communicationLayer;
@@ -50,7 +52,7 @@ public class AfekKuttenYungRunningState extends AfekKuttenYungState implements R
 
   public void startAlgorithm() throws IOException {
     safra.setActive(true, "Start AKY");
-    sendStateToAllNeighbours(new OurTimer());
+    sendDataToAllNeighbours(new OurTimer());
     try {
       synchronizer.awaitPulse();
     } catch (InterruptedException e) {
@@ -84,6 +86,7 @@ public class AfekKuttenYungRunningState extends AfekKuttenYungState implements R
     while (true) {
       step();
 
+      sendDataToAllNeighbours(timer);
       timer.stopAndCreateBasicTimeSpentEvent();
       setActive(false, "Step done");
       synchronizer.awaitPulse();
@@ -99,27 +102,206 @@ public class AfekKuttenYungRunningState extends AfekKuttenYungState implements R
   }
 
   private synchronized void step() throws IOException {
+    if (!(notRoot() && maxRoot()) && !iAmRoot()) {
+      becomeRoot();
+    } else if (!maxRoot()) {
+
+      boolean isAsking = false;
+      for (int i : neighbourData.keySet()) {
+        isAsking |= asking(i);
+      }
+
+      if (!isAsking) {
+        ask();
+      } else if (requesting() && granted(ownData.to)) {
+        join();
+      }
+    } else {
+      boolean isHandling = false;
+      for (int i : neighbourData.keySet()) {
+        isHandling |= handling(i) && request(i);
+      }
+      if (!isHandling) {
+        if (!notHandling()) {
+          resetRequest();
+        } else {
+          boolean isRequested = false;
+          int requestBy = AfekKuttenYungData.EMPTY_NODE;
+          for (int i : neighbourData.keySet()) {
+            isRequested |= request(i);
+            requestBy = i;
+          }
+          if (getParentData().from != me && isRequested) {
+            handleFor(requestBy);
+          }
+        }
+      } else if (iAmRoot() && ownData.direction == AfekKuttenYungData.ASK) {
+        grant();
+      } else if (granted(ownData.parent)) {
+        grant();
+      }
+    }
+
+
     // Implement algorithm from the book operating on own state and neighbour state
   }
 
-  private void updateOwnState(Message m, OurTimer timer) {
-    ownData.update(m);
-    sendStateUpdateToAllNeighbours(m, timer);
+  private void handleFor(int requestBy) {
+    AfekKuttenYungData otherData = neighbourData.get(requestBy);
+    ownData.req = otherData.req;
+    ownData.from = requestBy;
+    ownData.to = ownData.parent;
+    ownData.direction = AfekKuttenYungData.ASK;
+  }
+
+  private boolean notHandling() {
+    return ownData.req == AfekKuttenYungData.EMPTY_NODE
+        && ownData.from == AfekKuttenYungData.EMPTY_NODE
+        && ownData.to == AfekKuttenYungData.EMPTY_NODE
+        && ownData.direction == AfekKuttenYungData.EMPTY_DIRECTION;
+  }
+
+  private boolean handling(int i) {
+    AfekKuttenYungData data = neighbourData.get(i);
+    return data.req == ownData.req
+        && ownData.from == i
+        && data.to == me
+        && ownData.to == ownData.parent
+        && data.direction == AfekKuttenYungData.ASK;
+  }
+
+  private boolean request(int i) {
+    AfekKuttenYungData data = neighbourData.get(i);
+    return (isRoot(i)
+          && data.req == data.from
+          && data.from == i)
+        || (data.parent == me
+          && data.req != i
+          && data.req != AfekKuttenYungData.EMPTY_NODE);
+  }
+
+  private boolean isRoot(int i) {
+    AfekKuttenYungData data = i == me ? ownData : neighbourData.get(i);
+    return data.parent == AfekKuttenYungData.EMPTY_PARENT
+        && data.root == i
+        && data.distance == 0;
+  }
+
+  private void resetRequest() {
+    ownData.req = AfekKuttenYungData.EMPTY_NODE;
+    ownData.from = AfekKuttenYungData.EMPTY_NODE;
+    ownData.to = AfekKuttenYungData.EMPTY_NODE;
+    ownData.direction = AfekKuttenYungData.EMPTY_DIRECTION;
+  }
+
+  private void grant() {
+    ownData.direction = AfekKuttenYungData.GRANT;
+  }
+
+  private boolean granted(int grantingNeighbour) {
+    AfekKuttenYungData data = neighbourData.get(grantingNeighbour);
+    return ownData.req == data.req
+        && ownData.from == data.from
+        && data.direction == AfekKuttenYungData.GRANT
+        && ownData.direction == AfekKuttenYungData.ASK;
+  }
+
+  private boolean requesting() {
+    return neighbourData.keySet().contains(ownData.to)
+        && neighbourData.get(ownData.to).root > me
+        && ownData.req == me
+        && ownData.from == me;
+  }
+
+  private void join() {
+    ownData.parent = ownData.to;
+    ownData.root = neighbourData.get(ownData.to).root;
+    ownData.distance = neighbourData.get(ownData.to).distance + 1;
+    resetRequest();
+  }
+
+  private void ask() {
+    ownData.req = me;
+    ownData.from = me;
+    ownData.direction = AfekKuttenYungData.ASK;
+    ownData.to = getMaxRoot();
+  }
+
+  private int getMaxRoot() {
+    int maxRootValue = -1;
+    Set<Integer> maxRoots = new HashSet<>();
+
+    for (int n : neighbourData.keySet()) {
+      int rootValue = neighbourData.get(n).root;
+      if (rootValue > maxRootValue) {
+        maxRoots.clear();
+        maxRootValue = rootValue;
+      }
+      if (rootValue == maxRootValue) {
+        maxRoots.add(n);
+      }
+    }
+    return Collections.max(maxRoots);
+  }
+
+  private boolean asking(int i) {
+    return isMaxRoot(neighbourData.get(i).root)
+        && ownData.req == me
+        && ownData.from == me
+        && ownData.to == i
+        && ownData.direction == AfekKuttenYungData.ASK;
+  }
+
+  private boolean isMaxRoot(int root) {
+    for (AfekKuttenYungData data : neighbourData.values()) {
+      if (data.root > root) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private void becomeRoot() {
+    ownData.parent = AfekKuttenYungData.EMPTY_PARENT;
+    ownData.root = me;
+    ownData.distance = 0;
+  }
+
+  private boolean iAmRoot() {
+    return isRoot(me);
+  }
+
+  private boolean notRoot() {
+    return neighbourData.containsKey(ownData.parent)
+        && ownData.root > me
+        && ownData.root == getParentData().root
+        && ownData.distance == getParentData().distance + 1;
+  }
+
+  private boolean maxRoot() {
+    for (AfekKuttenYungData neighbour : neighbourData.values()) {
+      if (ownData.root < neighbour.root) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private AfekKuttenYungData getParentData() {
+    return neighbourData.get(ownData.parent);
   }
 
   private synchronized void copyNeighbourStates() {
     neighbourData = newNeighbourData;
     newNeighbourData = new HashMap<>();
     for (int i : communicationLayer.getNeighbours()) {
-      newNeighbourData.put(i, AfekKuttenYungData.getEmptyData());
+      if (!safra.crashDetected(i)) {
+        newNeighbourData.put(i, new AfekKuttenYungData(neighbourData.get(i)));
+      }
     }
   }
 
-  private void sendStateToAllNeighbours(OurTimer timer) {
-
-  }
-
-  private void sendStateUpdateToAllNeighbours(Message m, OurTimer timer) {
+  private void sendDataToAllNeighbours(OurTimer timer) {
 
   }
 
