dac177599ebcd3918938aa14aafd79fe6e2695b4
Per Fuchs <per@shiftleft.io>
2018-10-07 16:00:57 +0200

    Adds test for Chandy-Misra

--- ibisImplementation/small_test_aky.sh
+++ ibisImplementation/small_test_aky.sh
@@ -6,7 +6,7 @@ rm -rf /var/scratch/pfs250/test
 mkdir -p /var/scratch/pfs250/test
 
 echo "Running 50s"
-#./runChandyMisra.sh 1 50 2 /var/scratch/pfs250/test 0.00 fs aky 8888
+./runChandyMisra.sh 1 50 2 /var/scratch/pfs250/test 0.00 fs aky 8888
 ./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/test 0.00 ft aky 8888
 echo "5n"
 ./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/test 0.25 ft aky 8888
@@ -14,7 +14,7 @@ echo "90"
 ./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/test 0.90 ft aky 8888
 
 echo "Running 200s"
-#./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.00 fs aky 8888
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.00 fs aky 8888
 ./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.00 ft aky 8888
 echo "5n"
 ./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.25 ft aky 8888
---
+++ ibisImplementation/small_test_cm.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ant
+
+rm -rf /var/scratch/pfs250/test
+mkdir -p /var/scratch/pfs250/test
+
+echo "Running 50s"
+./runChandyMisra.sh 1 50 2 /var/scratch/pfs250/test 0.00 fs cm 8888
+./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/test 0.00 ft cm 8888
+echo "5n"
+./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/test 0.25 ft cm 8888
+echo "90"
+./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/test 0.90 ft cm 8888
+
+echo "Running 200s"
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.00 fs cm 8888
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.00 ft cm 8888
+echo "5n"
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.25 ft cm 8888
+echo "90"
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/test 0.90 ft cm 8888
