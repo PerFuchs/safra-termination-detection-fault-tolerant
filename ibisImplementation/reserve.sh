86db622f64cc91762d682f93954faf9b55943496
Per Fuchs <per@shiftleft.io>
2018-10-07 20:57:18 +0200

    Adds testing scripts for long evening runs

---
+++ ibisImplementation/reserve.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+preserve -t 08:00:00 -s 20:01 -np 20 > ~/reservation.txt
---
+++ ibisImplementation/test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+ant
+
+rm -rf /var/scratch/pfs250/big_test
+mkdir -p /var/scratch/pfs250/big_test
+
+reservation=`cat ~/reservation.txt`
+echo ${reservation}
+
+echo "Running 50s"
+echo "90"
+./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/big_test 0.90 ft aky 8888 "-reserve $reservation"
+echo "0.0"
+./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/big_test 0.00 fs aky 8888 "-reserve $reservation"
+./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/big_test 0.00 ft aky 8888 "-reserve $reservation"
+echo "5n"
+./runChandyMisra.sh 1 50 10 /var/scratch/pfs250/big_test 0.25 ft aky 8888 "-reserve $reservation"
+
+echo "Running 2000s"
+./runChandyMisra.sh 20 50 10 /var/scratch/pfs250/big_test 0.00 fs aky 8888 "-reserve $reservation"
+./runChandyMisra.sh 20 50 10 /var/scratch/pfs250/big_test 0.00 ft aky 8888 "-reserve $reservation"
+echo "5n"
+./runChandyMisra.sh 20 50 10 /var/scratch/pfs250/big_test 0.25 ft aky 8888 "-reserve $reservation"
+echo "90"
+./runChandyMisra.sh 20 50 10 /var/scratch/pfs250/big_test 0.90 ft aky 8888 "-reserve $reservation"
+
+
+echo "Running 200s"
+./runChandyMisra.sh 4 50 2 /var/scratch/pfs250/big_test 0.00 fs aky 8888 "-reserve $reservation"
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/big_test 0.00 ft aky 8888 "-reserve $reservation"
+echo "5n"
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/big_test 0.25 ft aky 8888 "-reserve $reservation"
+echo "90"
+./runChandyMisra.sh 4 50 10 /var/scratch/pfs250/big_test 0.90 ft aky 8888 "-reserve $reservation"
+
+
+
+
+
